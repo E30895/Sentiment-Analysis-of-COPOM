2af85e6208e04f0be26517417b619848ce3dd3c1
Nathan <99520642+E30895@users.noreply.github.com>
2023-07-21 11:57:13 -0300

    Update main.R

--- main.R
+++ main.R
@@ -12,14 +12,12 @@ source('get_atas.R')
 source('get_transform.R')
 source('get_Loughran_McDonald.R')
 source('sentiment_analysis.R')
+source('get_graphics.R')
 
-main = function(){
-  atas = get_atas()
-  atas_token = get_transform(atas)
-  Loughran_McDonald = get_Loughran_McDonald()
-  sentiment_analysis = sentiment_analysis(atas_token, Loughran_McDonald)
-  
-  return(sentiment_analysis)
-}
+atas = get_atas()
+atas_token = get_transform(atas)
+Loughran_McDonald = get_Loughran_McDonald()
+sentiment_analysis = sentiment_analysis(atas_token, Loughran_McDonald)
+ts = timeSeries(sentiment_analysis)
 
-analise_senetimentos = main()
+print(ts)
