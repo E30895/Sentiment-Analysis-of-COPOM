c2b55e9eef75c81f8389f4704fdb58ae8d377905
Nathan <99520642+E30895@users.noreply.github.com>
2023-08-06 22:04:37 -0300

    Update get_sentiment_analysis.R

--- get_sentiment_analysis.R
+++ get_sentiment_analysis.R
@@ -1,4 +1,4 @@
-sentiment_analysis = function(atas_token, dicionario){
+get_sentiment_analysis = function(atas_token, dicionario){
   
   '
   Essa função realiza a análise de sentimentos das atas do COPOM construindo
@@ -7,21 +7,21 @@ sentiment_analysis = function(atas_token, dicionario){
   Para a análise de sentimentos é utilizado o dicionário de finanças:
   Loughran-McDonald Master Dictionary w/ Sentiment Word Lists.
   '
+  
   atas_sentimentos = dplyr::inner_join(
     x = atas_token,
     y = dicionario,
   ) %>% 
-    dplyr::group_by(data) %>% 
+    dplyr::group_by(date) %>% 
     dplyr::count(ata, sentimento)
   
   analise_sentimento = atas_sentimentos %>% 
     tidyr::pivot_wider(
-      id_cols = "data",
+      id_cols = "date",
       names_from = "sentimento",
       values_from = "n",
     ) %>% 
     dplyr::mutate(sentimento_copom = ((positive - negative)/(positive+negative)))
-  
   
   return(analise_sentimento)
   
