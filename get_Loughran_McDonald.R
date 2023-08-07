191be9685d6f50a26a14e60471c1644927759d33
Nathan <99520642+E30895@users.noreply.github.com>
2023-08-06 22:04:23 -0300

    Update get_Loughran_McDonald.R

--- get_Loughran_McDonald.R
+++ get_Loughran_McDonald.R
@@ -1,9 +1,4 @@
 get_Loughran_McDonald = function(){
-
-  "
-  Essa função obtem o dicionário de Loughran and McDonald
-  e filtra apenas as palavras positivas e negativas
-  "
   
   dicionario = textdata::lexicon_loughran() %>% 
     dplyr::mutate(
@@ -14,7 +9,7 @@ get_Loughran_McDonald = function(){
     dplyr::group_by(token) %>% 
     dplyr::distinct(sentimento) %>%
     dplyr::ungroup() %>% 
-    dplyr::filter(sentimento %in% c('positive', 'negative'))
-                  
-    return(dicionario)
+    dplyr::filter(sentimento %in% c('positive', 'negative', 'uncertainty'))
+  
+  return(dicionario)
 }
