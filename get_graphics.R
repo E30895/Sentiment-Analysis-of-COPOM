7aee0b3eba33ce24613fbcad5ce2369e41b50707
Nathan <99520642+E30895@users.noreply.github.com>
2023-08-06 22:04:52 -0300

    Update get_graphics.R

--- get_graphics.R
+++ get_graphics.R
@@ -1,28 +1,64 @@
 
-timeSeries = function(data){
+get_graphics = function(sentiment_analysis){
   
-  ggplot2::ggplot(data) +
-    geom_line(
-      mapping  = aes(
-        x = data,
-        y = sentimento_copom)) +
+  '
+  Essa função abre os dados obtidos pelos processos anteiores
+  e obtem os resultados do NPL das atas do COPOM.
+  '
+  
+  hoje = Sys.Date() %>% as.Date()
+  hoje = format(hoje - 30, "%Y-%m") 
+  
+  sentiment_anualized = sentiment_analysis %>%
+    group_by(ano = lubridate::year(date)) %>%
+    summarise_all(mean)
+  
+  #GRÁFICO SENTIMENTOS ANUALISADOS
+  graphic_sentiment = ggplot(
+    sentiment_anualized, 
+    aes(x = ano))+
+    geom_line(aes(y = (positive)/(positive+negative+uncertainty), color = 'positive'), size = 0.8, linetype = 'longdash') +
+    geom_line(aes(y = (negative)/(positive+negative+uncertainty), color = 'negative'), size = 0.8, linetype = 'dotted') +
+    geom_line(aes(y = (uncertainty)/(positive+negative+uncertainty), color = 'uncertainty'), size = 0.8, linetype = 'twodash') +
+    ggplot2::annotate("text", x = 2008, y = 0.2, label = "2008", hjust = 0, color = "black")+
+    ggplot2::annotate("text", x = 2015, y = 0.2, label = "Recessao", hjust = 0, color = "black")+
+    ggplot2::annotate("text", x = 2018, y = 0.2, label = "Pandemia", hjust = 0, color = "black")+
     
-    #geom_vline(
-    #  xintercept = as.numeric(data$data[c(13)]),
-    #  linetype="dashed", 
-    #  color = "green", size=1.5)+
-    
-    #geom_text(
-    #  label = 'BARBA',
-    #  x = as.numeric(data$data[c(14)]),
-    #  y = c(0.2),
-    #  angle = 90, 
-  #  vjust = 1) +
+    labs(title = "Sentiment Series - Single Sentiment",
+         subtitle = paste("2000-01 to", hoje),
+         x = "Data",
+         y = "Valores (anualizados)",
+         color = "Indicador") +
+    scale_color_manual(values = c('positive' = '#0072B2', 'negative' = '#D55E00', 'uncertainty' = '#808080')) +
+    theme_minimal() +
+    theme(plot.title = element_text(size = 18, face = "bold"),
+          axis.text.x = element_text(angle = 45, hjust = 1),
+          legend.title = element_blank(),
+          legend.text = element_text(size = 12),
+          legend.position = "bottom")
   
-  labs(title = "Lorem ipsum",
-       subtitle = "Lorem ipsum",
-       caption = "Lorem ipsum")
   
-  #geom_vline(xintercept=as.numeric(tmp$x[c(13, 24)]),
-  #           linetype=4, colour="black")
+  #GRÁFICO SCORE MENSAL
+  graphic_score = ggplot(
+    sentiment_analysis,
+    aes(x = date))+
+    geom_line(aes(y = sentimento_copom, color = 'sentimento_copom'), size = 0.8, linetype = 'solid') +
+    ggplot2::annotate("text", x = as.Date('2008-01-01'), y = -0.4, label = "2008", hjust = 0, color = "black")+
+    ggplot2::annotate("text", x = as.Date('2014-04-14'), y = -0.4, label = "Recessao", hjust = 0, color = "black")+
+    ggplot2::annotate("text", x = as.Date('2019-12-11'), y = -0.6, label = "Pandemia", hjust = 0, color = "black")+
+    labs(title = "Sentiment Series - Score Sentiment",
+          subtitle = paste("2000-01 to", hoje),
+          x = "Data",
+          y = "Valores",
+          color = "Indicador") +
+    scale_color_manual(values = c('sentimento_copom' = '#0072B2')) +
+    theme_minimal() +
+    theme(plot.title = element_text(size = 18, face = "bold"),
+          axis.text.x = element_text(angle = 45, hjust = 1),
+          legend.title = element_blank(),
+          legend.text = element_text(size = 12),
+          legend.position = "bottom")
+
+  return(list(a = graphic_sentiment, b = graphic_score ))
+  
 }
