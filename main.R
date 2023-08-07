9dffb4ed46d0f8b28bbe40aff8a84c24ccd5ca55
Nathan <99520642+E30895@users.noreply.github.com>
2023-08-06 22:03:43 -0300

    Update main.R

--- main.R
+++ main.R
@@ -8,16 +8,16 @@ library(textdata)
 library(scales)
 library(ggtext)
 
+
 source('get_atas.R')
-source('get_transform.R')
+source('get_transform_atas.R')
 source('get_Loughran_McDonald.R')
 source('get_sentiment_analysis.R')
 source('get_graphics.R')
 
-atas = get_atas()
-atas_token = get_transform(atas)
-Loughran_McDonald = get_Loughran_McDonald()
-sentiment_analysis = sentiment_analysis(atas_token, Loughran_McDonald)
-ts = timeSeries(sentiment_analysis)
 
-print(ts)
+atas = get_atas()
+atas_token = get_transform_atas(atas)
+Loughran_McDonald = get_Loughran_McDonald()
+sentiment_analysis = get_sentiment_analysis(atas_token, Loughran_McDonald)
+grafico_ts = get_graphics(sentiment_analysis)
